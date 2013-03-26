6c6651c4b90828fa2fcf45376bbf98430536f25f
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-03-26 10:22:52 -0700

    Make Dist::Milla::App and properly override cmd and options rjbs++

---
+++ lib/Dist/Milla/App.pm
@@ -0,0 +1,24 @@
+package Dist::Milla::App;
+use strict;
+use parent 'Dist::Zilla::App';
+
+sub _default_command_base { 'Dist::Zilla::App::Command' }
+
+sub prepare_command {
+    my $self = shift;
+
+    my($cmd, $opt, @args) = $self->SUPER::prepare_command(@_);
+
+    if ($cmd->isa("Dist::Zilla::App::Command::install")) {
+        $opt->{install_command} ||= 'cpanm .';
+    } elsif ($cmd->isa("Dist::Zilla::App::Command::release")) {
+        $ENV{DZIL_CONFIRMRELEASE_DEFAULT} = 1
+          unless defined $ENV{DZIL_CONFIRMRELEASE_DEFAULT};
+    } elsif ($cmd->isa("Dist::Zilla::App::Command::new")) {
+        $opt->{provider} = 'Milla';
+    }
+
+    return $cmd, $opt, @args;
+}
+
+1;

b4a6faff5e2f83acb8090b4dcc6072090e9aad1d
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-09-18 08:45:14 +0900

    bump Test::More for more recent version

--- lib/Dist/Zilla/Plugin/Milla/MintFiles.pm
+++ lib/Dist/Zilla/Plugin/Milla/MintFiles.pm
@@ -43,7 +43,7 @@ requires 'perl', '5.008005';
 # requires 'Some::Module', 'VERSION';
 
 on test => sub {
-    requires 'Test::More', '0.88';
+    requires 'Test::More', '0.96';
 };
 ___[ t/basic.t ]___
 use strict;
